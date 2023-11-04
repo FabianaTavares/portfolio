ca834c1dfb8d6a5918611b4e2ce38a0721463667
Fabiana Tavares <fabi.tavares1@gmail.com>
2023-11-03 22:56:55 -0300

    feat: configure firebase
    configure firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { firebase } from './firebase.environment';
+
 export const environment = {
-  production: false
+  production: false,
+  ...firebase,
 };
 
 /*
@@ -14,3 +17,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
